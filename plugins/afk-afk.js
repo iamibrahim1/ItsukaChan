27d218b620a9c4dc8d3803d86a29368c40b6455e
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-06-17 07:27:03 -0700

    Update afk-afk.js

--- plugins/afk-afk.js
+++ plugins/afk-afk.js
@@ -2,7 +2,7 @@ let handler = async (m, { text }) => {
   let user = global.db.data.users[m.sender]
   user.afk = + new Date
   user.afkReason = text
-  m.reply(`${conn.getName(m.sender)} sekarang telah AFK dengan alasan ${text ? ': ' + text : ''}`)
+  m.reply(`${conn.getName(m.sender)} now been AFK with the grounds ${text ? ': ' + text : ''}`)
 }
 handler.help = ['afk'].map(v => v + ' <alasan>')
 handler.tags = ['main']
