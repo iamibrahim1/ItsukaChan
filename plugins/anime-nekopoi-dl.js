0fa1784ecf50dbfe0a754a4725e1edbd4b110b7d
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-06-17 07:30:03 -0700

    Update anime-nekopoi-dl.js

--- plugins/anime-nekopoi-dl.js
+++ plugins/anime-nekopoi-dl.js
@@ -19,14 +19,11 @@ cap += '' + '\n'
 	let arr = []
 	let tekss = res.episode.map(v => { return `${v.title}\nUpload : ${v.date || 'unknown'} || Id : ${v.id}`}).filter(v => v).join('\n\n')
 	// for (let x of res.episode) arr.push({ title: x.title, description: `Upload : ${x.date || 'unknown'} || Id : ${x.id}`, rowId: `${_p}nekodl ${x.id}` })
-	await m.reply(`*Title :* ${res.title}\n*Id :* ${res.id || '-'}\n*Date :* ${res.date ||'-'}\n*Japanese Title :* ${v.aliases || '-'}\n*Score :* ${v.skor || '-'}\n*Produser :* ${v.produser || '-'}\n*Status :* ${v.status || '-'}\n*Total Eps :* ${v.episode || '-'}\n*Durasi :* ${v.durasi || '-'}\n*Release :* ${v.tayang || '-'}\n*Genre :* ${v.genre || '-'}\n\n\n${tekss}`)
+	await m.reply(`*Title :* ${res.title}\n*Id :* ${res.id || '-'}\n*Date :* ${res.date ||'-'}\n*Japanese Title :* ${v.aliases || '-'}\n*Score :* ${v.skor || '-'}\n*Producer :* ${v.produser || '-'}\n*Status :* ${v.status || '-'}\n*Total Eps :* ${v.episode || '-'}\n*Duration :* ${v.durasi || '-'}\n*Release :* ${v.tayang || '-'}\n*Genre :* ${v.genre || '-'}\n\n\n${tekss}`)
 	// await conn.sendMessage(m.chat, { text: `*Title :* ${res.title}\n*Id :* ${res.id || '-'}\n*Date :* ${res.date ||'-'}\n*Japanese Title :* ${v.aliases || '-'}\n*Score :* ${v.skor || '-'}\n*Produser :* ${v.produser || '-'}\n*Status :* ${v.status || '-'}\n*Total Eps :* ${v.episode || '-'}\n*Durasi :* ${v.durasi || '-'}\n*Release :* ${v.tayang || '-'}\n*Genre :* ${v.genre || '-'}`, footer: null, title: null, buttonText: 'Click Here!', sections: [{ title: 'Nekopoi', rows: arr }] }, { quoted: m })
 	await apivisit
 	}
-	// By Chandra XD
-	// Follow bang
-	// TikTok : @pnggilajacn
-	// Github : https://github.com/Chandra-XD
+	
 	}
 handler.help = ['nekopoidl'].map(v => v + ' <id>')
 handler.tags = ['tools']
