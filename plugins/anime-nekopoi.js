db44f881c6ad7db4bafdaead6d19fb278515c635
Ibrahim <94687058+iamibrahim1@users.noreply.github.com>
2023-06-17 07:31:31 -0700

    Update anime-nekopoi.js

--- plugins/anime-nekopoi.js
+++ plugins/anime-nekopoi.js
@@ -13,7 +13,7 @@ let handler = async (m, { conn, args, usedPrefix: _p }) => {
 	await apivisit
 	} catch (e) {
 		console.log(e)
-		m.reply(`Terjadi kesalahan atau server sedang mengalami gangguan.`)
+		m.reply(`An error has occurred or the server is down.`)
 	}
 	// By Chandra XD
 	// Follow bang
